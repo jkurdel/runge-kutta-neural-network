155e4df0f62ae6c806fda4e5aeb78ea254aa045e
Jan Kurdel <jankurdel@gmail.com>
2012-07-25 20:10:00 +0200

    Add file which should be commited before with previous commit

--- matlab/levenberg_marquardt.m
+++ matlab/levenberg_marquardt.m
@@ -23,17 +23,28 @@ y = Theta(1) * gaussmf(x, [Theta(2), Theta(3)]);
 plot(x, y, 'r');
 
 % TODO: change the stop condition of gradient descent
-N = 1000;
-for i = 1:N
-    dy_dw = gaussmf(x, [Theta(2), Theta(3)]);
-    dy_dsigma = Theta(1) * (x - Theta(3)).^2 / (Theta(2)^3) .* y;
-    dy_dc = Theta(1) * (x - Theta(3)) / (Theta(2)^2) .* y;
-    
+N = 10000;
+n = 1;
+e_old = 1;
+e = 0;
+
+while (abs(sum(e_old.^2 - e.^2)) > 0.001) && (n < 10000)
+%     dy_dw = gaussmf(x, [Theta(2), Theta(3)]);
+%     dy_dsigma = Theta(1) * (x - Theta(3)).^2 / (Theta(2)^3) .* y;
+%     dy_dc = Theta(1) * (x - Theta(3)) / (Theta(2)^2) .* y;
+
+    dy_dw = 1 ./ exp((Theta(3) - x).^2 / (2*Theta(2)^2));
+    dy_dsigma = (Theta(1)*(Theta(3) - x).^2) ./ (Theta(2)^3*exp((Theta(3) - x).^2 / (2*Theta(2)^2)));
+    dy_dc = -(Theta(1)*(2*Theta(3) - 2*x)) ./ (2*Theta(2)^2*exp((Theta(3) - x).^2 / (2*Theta(2)^2)));
+
     Z = [dy_dw' dy_dsigma' dy_dc'];
+    e_old = e;
     e = f' - y';
 
     Theta = Theta + pinv(Z'*Z + 0.1*eye(3))*Z'*e;
     y = Theta(1) * gaussmf(x, [Theta(2), Theta(3)]);
+    
+    n = n + 1;
 end
 
-plot(x, y, 'r');
+plot(x, y, 'g');
